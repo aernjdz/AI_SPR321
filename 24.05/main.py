ff14458f41250d41afecf82dbb8db965191696bf
Aernjdz <zarichniukandriy5@gmail.com>
2025-05-24 10:41:32 +0300

    24.05 task

--- 24.05/main.py
+++ 24.05/main.py
@@ -1,9 +1,8 @@
 import pandas as pd
 from textblob import TextBlob
 
-# === 1. Завантаження CSV-файлу ===
 df = pd.read_csv("text.csv")  
-# === 2. Функція для аналізу тональності ===
+
 def classify_sentiment(text):
     blob = TextBlob(str(text))
     polarity = blob.sentiment.polarity
@@ -14,14 +13,11 @@ def classify_sentiment(text):
     else:
         return 'neutral'
 
-# === 3. Застосування моделі ===
 df['Sentiment'] = df['Message'].apply(classify_sentiment)
 
-# === 4. Підрахунок результатів ===
 result = df['Sentiment'].value_counts()
 
 print("Статистика аналізу тональності:")
 print(result)
 
-# (Опційно) Зберегти в CSV
 df.to_csv("analyzed_feedback.csv", index=False)
